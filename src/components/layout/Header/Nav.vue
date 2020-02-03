65be9f4a0b703421dd01595060616d66e518e80b
Nikita Makarin <n-makarin@mail.ru>
2020-02-03 19:41:35 +0300

    Create src\components\layout\Header\Nav.vue

---
+++ src/components/layout/Header/Nav.vue
@@ -0,0 +1,32 @@
+<template>
+  <nav class="nav">
+    <a
+      class="nav__link nav__github"
+      :href="links.github.path"
+      target="_blank"
+    >
+      {{ links.github.text }}
+    </a>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Nav',
+  components: {
+  },
+  data () {
+    return {
+      links: {
+        github: {
+          text: 'GitHub',
+          path: 'https://github.com/route/ferrum'
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
