c2b969540622f930663ced4a8b2a07964bf1e989
Nikita Makarin <n-makarin@mail.ru>
2020-02-03 20:31:17 +0300

    Create src\components\Proposal.vue

---
+++ src/components/Proposal.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="proposal">
+      <a :href="path" class="proposal__link">
+        <div class="proposal__text">{{ text }}</div>
+        <div class="proposal__logo">logo</div>
+      </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Proposal',
+  data () {
+    return {
+      text: 'Let’s try',
+      path: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
