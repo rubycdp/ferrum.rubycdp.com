67b4a33ec1914c25eeacc20a42711a4277cb8ab8
Nikita Makarin <n-makarin@mail.ru>
2020-02-03 20:05:19 +0300

    Create src\views\Home\sections\About.vue

---
+++ src/views/Home/sections/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    about
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  components: {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
