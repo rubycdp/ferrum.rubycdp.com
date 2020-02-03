0c6ae23536999cbfcfcd4a010ab6abfff41a3d5b
Nikita Makarin <n-makarin@mail.ru>
2020-02-03 19:41:27 +0300

    Create src\components\layout\Header\Logo.vue

---
+++ src/components/layout/Header/Logo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="logo">
+    logo
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Logo',
+  components: {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
