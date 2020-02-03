5d4277af66aae83b15bcbaec28935c48b801033b
Nikita Makarin <n-makarin@mail.ru>
2020-02-03 20:22:07 +0300

    Create src\components\layout\Footer.vue

---
+++ src/components/layout/Footer.vue
@@ -0,0 +1,31 @@
+<template>
+  <footer class="footer">
+    <div class="footer__copyright">{{ copyright }}</div>
+    <div class="footer__developer">
+      <span>{{ developer.siteBy }}</span>
+      <a :href="developer.link.path">{{ developer.link.text }}</a>
+    </div>
+  </footer>
+</template>
+
+<script>
+
+export default {
+  name: 'Footer',
+  data () {
+    return {
+      copyright: '©2020 Machinio',
+      developer: {
+        siteBy: 'Site by ',
+        link: {
+          path: 'http://evrone.com',
+          text: 'evrone.com'
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
