2d93e5704f5e7c3952cf3ca0c558384be686538a
Nikita Makarin <n-makarin@mail.ru>
2020-02-03 20:05:10 +0300

    Create src\views\Home\sections\Index.vue

---
+++ src/views/Home/sections/Index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="index">
+    index
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  components: {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
