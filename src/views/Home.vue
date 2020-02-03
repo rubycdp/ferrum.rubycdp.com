d9c14f7cbcc20218aba853cbed02f79dd1148954
Nikita Makarin <n-makarin@mail.ru>
2020-02-03 20:05:51 +0300

    Add sections components to src\views\Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,27 @@
 <template>
   <div class="home">
     <Header class="home__header" />
+    <div class="home__section-list">
+      <Index class="home__section home__section_index" />
+      <About class="home__section home__section_about" />
+      <Suggest class="home__section home__section_suggest" />
+    </div>
   </div>
 </template>
 
 <script>
 import Header from '@/components/layout/Header.vue'
+import Index from '@/views/Home/sections/Index.vue'
+import About from '@/views/Home/sections/About.vue'
+import Suggest from '@/views/Home/sections/Suggest.vue'
 
 export default {
   name: 'home',
   components: {
-    Header
+    Header,
+    Index,
+    About,
+    Suggest
   }
 }
 </script>
