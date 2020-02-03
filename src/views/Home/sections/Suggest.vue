7a05e2487453f92e86d839829fd05d7a839276c8
Nikita Makarin <n-makarin@mail.ru>
2020-02-03 20:05:28 +0300

    Create src\views\Home\sections\Suggest.vue

---
+++ src/views/Home/sections/Suggest.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="suggest">
+    suggest
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Suggest',
+  components: {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
