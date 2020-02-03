b474eb138cb080b7a47bc6ac6b39b13209a69d60
Nikita Makarin <n-makarin@mail.ru>
2020-02-03 19:42:51 +0300

    Add Header.vue to src\views\Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,19 @@
 <template>
   <div class="home">
+    <Header class="home__header" />
   </div>
 </template>
 
 <script>
+import Header from '@/components/layout/Header.vue'
+
 export default {
+  name: 'home',
   components: {
+    Header
   }
 }
 </script>
+
+<style lang="scss" scoped>
+</style>
