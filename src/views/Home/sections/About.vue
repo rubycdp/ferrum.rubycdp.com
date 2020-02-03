0080bdd432f9b6a5e210739dad4c3cf27a8d8ae3
Nikita Makarin <n-makarin@mail.ru>
2020-02-03 21:00:21 +0300

    Add markup to src\views\Home\sections\About.vue

--- src/views/Home/sections/About.vue
+++ src/views/Home/sections/About.vue
@@ -1,13 +1,32 @@
 <template>
   <div class="about">
-    about
+    <h1 class="about__title">{{ title }}</h1>
+    <a
+      class="about__link"
+      :href="links.ferrum.path"
+    >
+      {{ links.ferrum.text }}
+    </a>
+    <div class="about__scroll">{{ explore }}</div>
   </div>
 </template>
 
 <script>
+import links from '@/data/links.js'
+
 export default {
   name: 'About',
-  components: {
+  data () {
+    return {
+      title: 'Fearless Ruby Chrome driver',
+      links: {
+        ferrum: {
+          text: 'Go to GitHub',
+          path: links.ferrum
+        }
+      },
+      explore: 'Explore'
+    }
   }
 }
 </script>
