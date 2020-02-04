fc2a5346a99de1faeaa8a356476063b5e1066dc1
Nikita Makarin <n-makarin@mail.ru>
2020-02-04 03:23:50 +0300

    Replace markup from About to Index section

--- src/views/Home/sections/About.vue
+++ src/views/Home/sections/About.vue
@@ -1,31 +1,13 @@
 <template>
   <div class="about">
-    <h1 class="about__title">{{ title }}</h1>
-    <a
-      class="about__link"
-      :href="links.ferrum.path"
-    >
-      {{ links.ferrum.text }}
-    </a>
-    <div class="about__scroll">{{ explore }}</div>
   </div>
 </template>
 
 <script>
-import links from '@/data/links.js'
-
 export default {
   name: 'About',
   data () {
     return {
-      title: 'Fearless Ruby Chrome driver',
-      links: {
-        ferrum: {
-          text: 'Go to GitHub',
-          path: links.ferrum
-        }
-      },
-      explore: 'Explore'
     }
   }
 }
--- src/views/Home/sections/Index.vue
+++ src/views/Home/sections/Index.vue
@@ -1,13 +1,32 @@
 <template>
   <div class="index">
-    index
+    <h1 class="index__title">{{ title }}</h1>
+    <a
+      class="index__link"
+      :href="links.ferrum.path"
+    >
+      {{ links.ferrum.text }}
+    </a>
+    <div class="index__scroll">{{ explore }}</div>
   </div>
 </template>
 
 <script>
+import links from '@/data/links.js'
+
 export default {
   name: 'Index',
-  components: {
+  data () {
+    return {
+      title: 'Fearless Ruby Chrome driver',
+      links: {
+        ferrum: {
+          text: 'Go to GitHub',
+          path: links.ferrum
+        }
+      },
+      explore: 'Explore'
+    }
   }
 }
 </script>
