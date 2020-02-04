570732b00afea5d00e316fb50b9d32da6daf50a5
Nikita Makarin <n-makarin@mail.ru>
2020-02-04 13:54:17 +0300

    Add rule for svg loader to vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,18 @@
 module.exports = {
-  // webpack autofix rule
   chainWebpack: config => {
+    // webpack autofix rule
     config.module.rule('eslint').use('eslint-loader').options({
       fix: true
     })
+    // svg-to-vue rule
+    const svgRule = config.module.rule('svg');
+    svgRule.uses.clear();
+    svgRule
+      .use('babel-loader')
+      .loader('babel-loader')
+      .end()
+      .use('vue-svg-loader')
+      .loader('vue-svg-loader');
   },
   css: {
     // glabal styles
