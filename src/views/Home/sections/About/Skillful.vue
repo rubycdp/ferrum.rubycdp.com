60d56c3841ac61a74d6e16cbcc8fd331aaa61d29
Nikita Makarin <n-makarin@mail.ru>
2020-02-04 03:53:22 +0300

    Create src\views\Home\sections\About\Skillful.vue

---
+++ src/views/Home/sections/About/Skillful.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="skillful">
+    <h2 class="skillful__title">{{ title }}</h2>
+    <div class="skillful__skills">
+      <div
+        v-for="(skill, key) in skills"
+        :key="key"
+      >
+        {{ skill }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Skillful',
+  data () {
+    return {
+      title: 'Skillful',
+      skills: [
+        'Navigation & finders', 'Screenshots', 'Mouse & keyboard',
+        'Network, cookies, headers', 'JS, frames, dialogs'
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
