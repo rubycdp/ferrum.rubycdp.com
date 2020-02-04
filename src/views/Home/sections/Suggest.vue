428c740b987edca326b121cc86fae70c63e1ec23
Nikita Makarin <n-makarin@mail.ru>
2020-02-04 04:04:02 +0300

    Add markup to src\views\Home\sections\Suggest.vue

--- src/views/Home/sections/Suggest.vue
+++ src/views/Home/sections/Suggest.vue
@@ -1,13 +1,55 @@
 <template>
   <div class="suggest">
-    suggest
+    <h2 class="suggest__title">{{ title }}</h2>
+    <div class="suggest__service-list">
+      <div
+        class="suggest__service suggest-service"
+        v-for="(service, key) in services"
+        :key="key"
+      >
+        <div class="suggest-service__name">{{ service.name }}</div>
+        <div class="suggest-service__description">{{ service.description }}</div>
+        <a
+          :href="service.link.path"
+          target="_blank"
+          class="suggest-service__link"
+        >
+          {{ service.link.text }}
+        </a>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import links from '@/data/links.js'
+
 export default {
   name: 'Suggest',
-  components: {
+  data () {
+    return {
+      title: 'USEFUL',
+      services: [
+        {
+          name: 'Cuprite',
+          description: `Is a Capybara driver which uses Ferrum underneath.
+            Basically it makes Ferrum to be compatible with Capybara calls`,
+          link: {
+            text: 'Github',
+            path: links.cuprite
+          }
+        },
+        {
+          name: 'Vessel',
+          description: `In this simple framework you can build and start running
+            in parallel your crawler which works on top of Ferrum`,
+          link: {
+            text: 'Github',
+            path: links.vessel
+          }
+        }
+      ]
+    }
   }
 }
 </script>
