55a9d35df970a85f90a6553906e87c6c9b7488c6
Nikita Makarin <n-makarin@mail.ru>
2020-02-04 13:54:41 +0300

    Add svg logo to components\layout\Header\Logo.vue

--- src/components/layout/Header/Logo.vue
+++ src/components/layout/Header/Logo.vue
@@ -1,16 +1,24 @@
 <template>
   <div class="logo">
-    logo
+    <SVGLogo />
   </div>
 </template>
 
 <script>
+import SVGLogo from '@/assets/images/logo.svg'
+
 export default {
   name: 'Logo',
   components: {
+    SVGLogo
   }
 }
 </script>
 
 <style lang="scss" scoped>
+.logo {
+  path {
+    fill: $white;
+  }
+}
 </style>
