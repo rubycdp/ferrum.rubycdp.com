108391723af03acca4ea3ef247587bdaa2394c14
Nikita Makarin <n-makarin@mail.ru>
2020-02-04 04:57:07 +0300

    Add vueSmoothScroll to main.js

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vueSmoothScroll from 'vue2-smooth-scroll'
+
+Vue.use(vueSmoothScroll)
 
 Vue.config.productionTip = false
 
