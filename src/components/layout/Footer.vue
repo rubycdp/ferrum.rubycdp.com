c0a9d83d088e9bfba78089e8a95c2495b511c0d9
Nikita Makarin <n-makarin@mail.ru>
2020-02-04 07:27:01 +0300

    Add styles to Footer.vue src\components\layout\Footer.vue

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="footer">
-    <div class="footer__copyright">{{ copyright }}</div>
-    <div class="footer__developer">
+    <div class="footer__copyright color-dim">{{ copyright }}</div>
+    <div class="footer__developer color-dim">
       <span>{{ developer.siteBy }}</span>
       <a target="_blank" :href="developer.link.path">{{ developer.link.text }}</a>
     </div>
@@ -29,4 +29,23 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.footer {
+  display: flex;
+  padding: 48px 0;
+  &__copyright, &__developer, &__developer a {
+    font-size: 16px;
+    line-height: 1.6;
+  }
+  .color-dim {
+    opacity: .3;
+  }
+  &__copyright {
+    margin-right: 24px;
+  }
+  &__developer {
+    a {
+      color: $white;
+    }
+  }
+}
 </style>
