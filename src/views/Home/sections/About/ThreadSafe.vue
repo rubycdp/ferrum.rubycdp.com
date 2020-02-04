38a87e802c48f1d9eda9d1743d93a5995ef87a02
Nikita Makarin <n-makarin@mail.ru>
2020-02-04 03:53:34 +0300

    Create views\Home\sections\About\ThreadSafe.vue

---
+++ src/views/Home/sections/About/ThreadSafe.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="thread-safe">
+    <h2 class="thread-safe__title">{{ title }}</h2>
+    <p class="thread-safe__text">{{ text }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ThreadSafe',
+  data () {
+    return {
+      title: 'Thread safe',
+      text: `Ferrum was written to be fully thread safe from the beginning. 
+        Each page can be maintained independently through a dedicated websocket`
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
