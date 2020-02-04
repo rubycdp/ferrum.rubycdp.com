a8634dede1940d495c17bcaf604451cd52d5f7e4
Nikita Makarin <n-makarin@mail.ru>
2020-02-04 16:34:18 +0300

    Add svg title icon in ThreadSafe.vue src\views\Home\sections\About\ThreadSafe.vue

--- src/views/Home/sections/About/ThreadSafe.vue
+++ src/views/Home/sections/About/ThreadSafe.vue
@@ -1,13 +1,21 @@
 <template>
   <div class="thread-safe">
-    <h2 class="thread-safe__title">{{ title }}</h2>
+    <div class="thread-safe__title-wrapper">
+      <h2 class="thread-safe__title">{{ title }}</h2>
+      <SVGThreadSaveTitle />
+    </div>
     <p class="thread-safe__text">{{ text }}</p>
   </div>
 </template>
 
 <script>
+import SVGThreadSaveTitle from '@/assets/images/home/thread_safe_title.svg'
+
 export default {
   name: 'ThreadSafe',
+  components: {
+    SVGThreadSaveTitle
+  },
   data () {
     return {
       title: 'Thread safe',
@@ -20,6 +28,16 @@ export default {
 
 <style lang="scss" scoped>
 .thread-safe {
+  &__title-wrapper {
+    position: relative;
+    display: inline-block;
+    svg {
+      position: absolute;
+      right: -64px;
+      top: 50%;
+      transform: translateY(-45%);
+    }
+  }
   &__text {
     max-width: 665px;
     line-height: 1.6;
