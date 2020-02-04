4cafbd489edf7512c40768f06caf19f9de224a4c
Nikita Makarin <n-makarin@mail.ru>
2020-02-04 03:54:22 +0300

    Add internal components to About.vue src\views\Home\sections\About.vue

--- src/views/Home/sections/About.vue
+++ src/views/Home/sections/About.vue
@@ -1,11 +1,23 @@
 <template>
   <div class="about">
+    <Meet class="about__block" />
+    <Skillful class="about__block" />
+    <ThreadSafe class="about__block" />
   </div>
 </template>
 
 <script>
+import Meet from '@/views/Home/sections/About/Meet'
+import Skillful from '@/views/Home/sections/About/Skillful'
+import ThreadSafe from '@/views/Home/sections/About/ThreadSafe'
+
 export default {
   name: 'About',
+  components: {
+    Meet,
+    Skillful,
+    ThreadSafe
+  },
   data () {
     return {
     }
