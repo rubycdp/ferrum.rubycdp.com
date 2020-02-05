0929dd9ac6059d99ac1c40f4dbe523f0013b50eb
Nikita Makarin <n-makarin@mail.ru>
2020-02-05 16:43:16 +0300

    Set Docs.vue page preloading in route/index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Docs from '../views/Docs.vue'
 
 Vue.use(VueRouter)
 
@@ -13,7 +14,7 @@ const routes = [
   {
     path: '/docs',
     name: 'docs',
-    component: () => import('../views/Docs.vue')
+    component: Docs
   }
 ]
 
