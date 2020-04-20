9c8602344bf147f05a2a3deb8dc1cc21f8415a57
Nikita Makarin <zn.makarin@gmail.com>
2020-04-20 15:16:30 +0300

    Change text in Meet section

--- src/views/Home/sections/About/Meet.vue
+++ src/views/Home/sections/About/Meet.vue
@@ -28,9 +28,9 @@ export default {
   data () {
     return {
       title: 'Meet',
-      text: `Ferrum provides you a high level API interface to Chrome browser and allows you
-        to fully automate it and grab data from any public site you want. There’s no additional
-        software required — blend 1/2 pure Chrome with 1/2 pure Ruby. Exciting mix. No addiction!`,
+      text: `Ferrum provides you a high level API interface to Chrome browser and allows 
+        you to fully automate it and grab data from any public site you want. 
+        There’s no additional software required — only Chrome, Ruby and you.`,
       advantages: [
         {
           text: 'Pure Chrome',
@@ -90,7 +90,7 @@ export default {
     }
   }
   &__text {
-    max-width: 653px;
+    max-width: 537px;
     line-height: 32px;
     margin-bottom: 95px;
     margin-top: 0;
