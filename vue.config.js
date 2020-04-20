c263ec9008194618c0bc8709b8ca8002f9ced3bb
Nikita Makarin <zn.makarin@gmail.com>
2020-04-20 18:15:57 +0300

    Linter autofix

--- vue.config.js
+++ vue.config.js
@@ -5,14 +5,14 @@ module.exports = {
       fix: true
     })
     // svg-to-vue rule
-    const svgRule = config.module.rule('svg');
-    svgRule.uses.clear();
+    const svgRule = config.module.rule('svg')
+    svgRule.uses.clear()
     svgRule
       .use('babel-loader')
       .loader('babel-loader')
       .end()
       .use('vue-svg-loader')
-      .loader('vue-svg-loader');
+      .loader('vue-svg-loader')
   },
   css: {
     // glabal styles
